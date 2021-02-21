9b3c945adfafb5d3a12662692d086c05cf5940d6
chezhe <chezhe17@gmail.com>
2021-02-21 16:16:48 +0800

    Revert "rm config for the moment"
    This reverts commit 8e594eee91646abcce03c27ebc8249365e6a8229.

--- packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
+++ packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# echo_config() {
-#   echo '{'
-#   echo '  "frontendTag": "'"$FRONTEND_TAG"'",'
-#   echo '  "infuraApiKey": "'"$INFURA_API_KEY"'"'
-#   echo '}'
-# }
+echo_config() {
+  echo '{'
+  echo '  "frontendTag": "'"$FRONTEND_TAG"'",'
+  echo '  "infuraApiKey": "'"$INFURA_API_KEY"'"'
+  echo '}'
+}
 
-# touch ./public/config.json
+touch ./public/config.json
 
-# echo_config > ./public/config.json
-
-rm ./public/config.json
+echo_config > ./public/config.json
 
 exit 0
