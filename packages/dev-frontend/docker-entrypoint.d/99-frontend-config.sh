8e594eee91646abcce03c27ebc8249365e6a8229
chezhe <chezhe17@gmail.com>
2021-02-21 16:01:13 +0800

    rm config for the moment

--- packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
+++ packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo_config() {
-  echo '{'
-  echo '  "frontendTag": "'"$FRONTEND_TAG"'",'
-  echo '  "infuraApiKey": "'"$INFURA_API_KEY"'"'
-  echo '}'
-}
+# echo_config() {
+#   echo '{'
+#   echo '  "frontendTag": "'"$FRONTEND_TAG"'",'
+#   echo '  "infuraApiKey": "'"$INFURA_API_KEY"'"'
+#   echo '}'
+# }
 
-touch ./public/config.json
+# touch ./public/config.json
 
-echo_config > ./public/config.json
+# echo_config > ./public/config.json
+
+rm ./public/config.json
 
 exit 0
